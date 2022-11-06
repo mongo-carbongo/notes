c93acf9069e64097f94e05157f958084950e304f
mauro <killuazaoldieck85@gmail.com>
2022-11-06 07:30:28 -0500

    November 6 2022 second

--- developer/test_dome/filter.rb
+++ developer/test_dome/filter.rb
@@ -1,8 +1,13 @@
-Implement the filter method. It should accept a list of students and a variable number of filters. The filters will be passed in as lambdas, each of which will take one student record and return a boolean value.
+Implement the filter method. It should accept a list of students and a variable
+number of filters. The filters will be passed in as lambdas, each of which
+will take one student record and return a boolean value.
 
-filter should return a list of student records that have passed through all the filters.
+filter should return a list of student records that have passed through all the
+ filters.
 
-If no filters are provided, the list of student records should be returned as is. If no students pass through all the provided filters (or if no students are provided), return an empty array.
+If no filters are provided, the list of student records should be returned
+as is. If no students pass through all the provided filters (or if no students
+  are provided), return an empty array.
 
 For example, consider the following code:
 
